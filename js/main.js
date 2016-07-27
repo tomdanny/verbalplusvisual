931769a5af05259ce428fa191fa9c5c38a7c2177
Daniel Tomic <tom_danny2003@yahoo.com>
2016-07-27 18:32:23 -0400

    sidebar added

--- js/main.js
+++ js/main.js
@@ -3,10 +3,42 @@
    ====================================================== */
 
 $(document).ready(function(){
-   $(".arrow").click(function(){
+   $(".arrow").click(function(e){
+      e.preventDefault();
       $('html, body').animate({
        scrollTop: $($(this).attr('href')).offset().top
       }, 1000);
    });
 });
 
+
+
+/* ======================================================
+                      NAVBAR
+   ====================================================== */
+
+   $(document).ready(function(){
+     $(window).bind('scroll', function() {
+     var navHeight = $( window ).height() - 50;
+       if ($(window).scrollTop() > navHeight) {
+         $('nav').addClass('fixed');
+       }
+       else {
+         $('nav').removeClass('fixed');
+       }
+    });
+  });
+
+
+  $(document).ready(function () {
+    $(".navbar-toggle").on("click", function () {
+        $(this).toggleClass("active");
+    });
+  });
+
+
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+    // $('html, body').toggleClass("toggleScroll");
+});
