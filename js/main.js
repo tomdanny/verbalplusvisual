0758e9b280539261fe3bd12931f6aa7844749ba2
Daniel Tomic <tom_danny2003@yahoo.com>
2016-07-27 11:30:39 -0400

    arrow on hover/click animation

--- js/main.js
+++ js/main.js
@@ -1,20 +1,12 @@
 /* ======================================================
-   fixed navbar
+                      ARROW
    ====================================================== */
-$(document).ready(function() {
-  
-  $(window).scroll(function () {
-      //if you hard code, then use console
-      //.log to determine when you want the 
-      //nav bar to stick.  
-      // console.log($(window).scrollTop())
-    if ($(window).scrollTop() > 125) {
-      $('.navbar').addClass('navbar-fixed');
-      $('.layout-1').addClass('add-layouts-padding');
-    }
-    if ($(window).scrollTop() < 126) {
-      $('.navbar').removeClass('navbar-fixed');
-      $('.layout-1').removeClass('add-layouts-padding');
-    }
-  });
+
+$(document).ready(function(){
+   $(".arrow").click(function(){
+      $('html, body').animate({
+       scrollTop: $($(this).attr('href')).offset().top
+      }, 1000);
+   });
 });
+
