7e90854b327c6503be79d712d4db11186a608d37
Daniel Tomic <tom_danny2003@yahoo.com>
2016-07-28 10:35:45 -0400

    second image fades in on scroll down

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@
                       ARROW
    ====================================================== */
 
-$(document).ready(function(){
-   $(".arrow").click(function(e){
-      e.preventDefault();
-      $('html, body').animate({
-       scrollTop: $($(this).attr('href')).offset().top
-      }, 1000);
-   });
-});
+  $(document).ready(function(){
+     $(".arrow").click(function(e){
+        e.preventDefault();
+        $('html, body').animate({
+         scrollTop: $($(this).attr('href')).offset().top
+        }, 1000);
+     });
+  });
 
 
 
@@ -42,3 +42,39 @@ $("#menu-toggle").click(function(e) {
     $("#wrapper").toggleClass("toggled");
     // $('html, body').toggleClass("toggleScroll");
 });
+
+
+
+
+/* ======================================================
+                    SECOND IMAGE LOAD
+   ====================================================== */
+
+
+$(document).ready(function () {
+        function showImages(el) {
+        var windowHeight = jQuery( window ).height();
+        $(el).each(function(){
+            var thisPos = $(this).offset().top;
+
+            var topOfWindow = $(window).scrollTop();
+            if (topOfWindow + windowHeight - 350 > thisPos ) {
+                $(this).addClass("fadeIn");
+            }
+        });
+    }
+
+    // if the image in the window of browser when the page is loaded, show that image
+    $(document).ready(function(){
+            showImages('.star');
+    });
+
+    // if the image in the window of browser when scrolling the page, show that image
+    $(window).scroll(function() {
+            showImages('.star');
+    });
+  });
+
+
+
+
